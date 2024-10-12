d16dac1c96139a18f1d2402ab32d1dd862e94976
asdf <asdf@gmail.com>
2024-10-12 18:06:33 +0500

    commit1

--- js/family-slider.js
+++ js/family-slider.js
@@ -1,27 +1,26 @@
-// JavaScript to control the family member slider
 const slides = document.querySelectorAll('.family-slide');
 let currentSlide = 0;
-
-// Show the first slide initially
 showSlide(currentSlide);
 
-// Function to show a specific slide
+// function that show the specific slide
 function showSlide(index) {
-    // Hide all slides
+    //  this hie other slides
     slides.forEach(slide => {
         slide.classList.remove('active');
     });
 
-    // Show the current slide
+    // show the current slide
     slides[index].classList.add('active');
 }
 
-// Event listeners for next and previous buttons
+// btn for nextBtn
 document.getElementById('nextBtn').addEventListener('click', () => {
     currentSlide = (currentSlide + 1) % slides.length; // Go to the next slide
     showSlide(currentSlide);
 });
 
+
+// btn for nextBtn
 document.getElementById('prevBtn').addEventListener('click', () => {
     currentSlide = (currentSlide - 1 + slides.length) % slides.length; // Go to the previous slide
     showSlide(currentSlide);
